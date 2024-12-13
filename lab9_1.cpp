c7c80422d0521a7f1bf99ad6be0261a691567a6a
rzryr1 <solotogre@gmail.com>
2024-12-13 16:13:40 +0700

    lab9_1.cpp completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -2,10 +2,22 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
+double loan;
+double percent;
+double py;
+double tt;
+double nb;
+double rate1 ;
+double rate2 ;
+int year=1;
 int main(){	
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> percent;
 	cout << "Enter amount you can pay per year: ";
+	cin >> py;	
+	double rate1 = (percent/100);	
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +32,29 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	do
+	{
+	cout << fixed << setprecision(2);		
+	cout << setw(13) << left << year; 
+	cout << setw(13) << left << loan;	
+	double rate2 = (rate1*loan);
+	cout << setw(13) << left << rate2;
+	double tt = loan+rate2;
+	cout << setw(13) << left << tt;
+	if(tt<py){
+		py=tt;
+	}
+	cout << setw(13) << left << py;	
+	loan=tt-py;
+	cout << setw(13) << left << loan;
+	cout << "\n";
+	year++;
+		
+	} while (loan >0 );
+	
 	
 	return 0;
 }
+	
+
+
