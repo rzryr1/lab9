fb7f5c5cdb4118e0add92085cfe1631bc352b1be
rzryr1 <solotogre@gmail.com>
2024-12-13 16:13:55 +0700

    lab9_2.cpp completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,7 +2,16 @@
 using namespace std;
 
 //Write the function printO() here
-
+void printO(int N ,int M){
+	if(N <= 0 || M <= 0){
+		cout << "Invalid input";
+	}
+	for(int i=0; i<N; i++){
+		for(int j=0; j<M; j++){
+			cout << "O";
+		}cout << "\n";
+	}
+}
 int main(){
 	
 	printO(2,2);
@@ -22,3 +31,10 @@ int main(){
 	
 	return 0;
 }
+// void printO(N ,M){
+// 	for(int i=0; i<N; i++){
+// 		for(int j=0; j<M; j++){
+// 			cout << "O";
+// 		}cout << "\n";
+// 	}
+// }
